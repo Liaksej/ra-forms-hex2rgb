cbd866ce11bb4c06bd5df65e5417b1caf852b481
Liaksej <tarusov.liaksej@gmail.com>
2023-09-27 18:46:53 +0200

    Refactored Home component, added hex color to RGB conversion
    Removed previously unrelated content of the Home function component from src/app/page.tsx. Incorporated a hex color code to RGB conversion tool in the file. Input of hexadecimal color code now changes background color of the page. Any invalid hexadecimal will set the background as red.
    
    This change was essential to enhance the utility of the page by including a commonly used tool for developers and designers. The error handling for invalid inputs improves the resilience of the code and improves user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,38 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
+
+function translateHexToRGB(hex: string) {
+  if (!/^#[0-9A-Fa-f]+$/g.test(hex)) {
+    return "Ошибка!";
+  }
+
+  hex = hex.replace("#", "");
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 export default function Home() {
+  const [color, setColor] = useState("");
+
+  const hexHandler = (e: any) => {
+    e.preventDefault();
+    if (e.target.value.length >= 7) {
+      const rgbaString = translateHexToRGB(e.target.value);
+      rgbaString === "Ошибка!"
+        ? setColor("rgba(255, 69, 0)")
+        : setColor(rgbaString);
+    } else setColor("");
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div style={{ backgroundColor: `${color}` }}>
+      <input type="text" onInput={(e) => hexHandler(e)} />
+      <div>{color === "rgba(255, 69, 0)" ? "Ошибка!" : color}</div>
+    </div>
+  );
 }
